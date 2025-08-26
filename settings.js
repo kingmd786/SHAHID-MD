636131b3db4f58a85c066e63a146d813bbf81b71
kingmd786 <shahidkingmd874@gmail.com>
2025-08-26 14:56:14 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '✨PATHAN-BOT✨',
+  author: '👑 Shafi PATHAN🇦🇫',
+  botName: '🤖 PATHAN BOT',
+  botOwner: 'Shafi PATHAN🇦🇫', // Your name here u can change 
+  ownerNumber: '93703802176', // Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: 'public', // can be 'public' or 'private'
+  description: '💬 A powerful fun WhatsApp MD bot built with ❤️ by pathan power.',
+  version: '2.0.0',
+};
+
+module.exports = settings;
